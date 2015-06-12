3f4f10b284c2e87edca45bffc5d378b15823ab2b
joaobiriba <glaquidara@gmail.com>
2015-06-12 22:14:03 +0200

    Using spotify wep api repository; Custom Adapter; keyboard adjustPan in Manifest

---
+++ app/src/main/java/com/laquysoft/spotifystreamer/ArtistAdapter.java
@@ -0,0 +1,57 @@
+package com.laquysoft.spotifystreamer;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import kaaes.spotify.webapi.android.models.Artist;
+
+/**
+ * Created by joaobiriba on 12/06/15.
+ */
+public class ArtistAdapter extends ArrayAdapter<Artist> {
+
+
+    private class ViewHolder {
+        public TextView name;
+        public ImageView thumbnail;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder = null;
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) getContext()
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.list_item_artist, null);
+            viewHolder = new ViewHolder();
+            viewHolder.name = (TextView)convertView.findViewById(R.id.list_item_artist_textview);
+            viewHolder.thumbnail = (ImageView)convertView.findViewById(R.id.thumbnail);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        Artist artist = getItem(position);
+        viewHolder.name.setText(artist.name);
+
+        if ( artist.images.size() > 0 ) {
+            Picasso.with(parent.getContext()).load(artist.images.get(0).url).into(viewHolder.thumbnail);
+        }
+
+        return convertView;
+    }
+
+
+    public ArtistAdapter(Context context, int resource,
+                         List<Artist> artistList) {
+        super(context, resource, artistList);
+    }
+}
--- app/src/main/java/com/laquysoft/spotifystreamer/ArtistsFragment.java
+++ app/src/main/java/com/laquysoft/spotifystreamer/ArtistsFragment.java
@@ -4,13 +4,9 @@
 package com.laquysoft.spotifystreamer;
 
 
-import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,26 +18,21 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.List;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Artist;
+import kaaes.spotify.webapi.android.models.Artists;
+import kaaes.spotify.webapi.android.models.ArtistsPager;
 
 /**
  * Encapsulates fetching the artists and displaying them in a list .
  */
 public class ArtistsFragment extends Fragment {
 
-    private ArrayAdapter<String> mArtistsAdapter;
+    private ArtistAdapter mArtistsAdapter;
     private EditText artistEditText;
 
     public ArtistsFragment() {
@@ -60,16 +51,12 @@ public class ArtistsFragment extends Fragment {
 
         // The ArrayAdapter will take data from a source and
         // use it to populate the ListView it's attached to.
-        mArtistsAdapter =
-                new ArrayAdapter<String>(
-                        getActivity(), // The current context (this activity)
-                        R.layout.list_item_artist, // The name of the layout ID.
-                        R.id.list_item_artist_textview, // The ID of the textview to populate.
-                        new ArrayList<String>());
+        mArtistsAdapter = new ArtistAdapter(getActivity(), R.layout.list_item_artist, new ArrayList<Artist>());
+
 
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-         artistEditText = (EditText) rootView.findViewById(R.id.input_artist);
+        artistEditText = (EditText) rootView.findViewById(R.id.input_artist);
 
         artistEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
@@ -90,7 +77,7 @@ public class ArtistsFragment extends Fragment {
 
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                String artist = mArtistsAdapter.getItem(position);
+                //String artist = mArtistsAdapter.getItem(position).name;
                 //Intent intent = new Intent(getActivity(), DetailActivity.class)
                 //      .putExtra(Intent.EXTRA_TEXT, forecast);
                 //startActivity(intent);
@@ -112,139 +99,36 @@ public class ArtistsFragment extends Fragment {
         //updateArtists();
     }
 
-    public class FetchArtistsTask extends AsyncTask<String, Void, String[]> {
+    public class FetchArtistsTask extends AsyncTask<String, Void, List<Artist>> {
 
         private final String LOG_TAG = FetchArtistsTask.class.getSimpleName();
 
 
-        /**
-         * Take the String representing the complete output for the artist in JSON Format and
-         * pull out the data we need to construct the Strings needed for the wireframes.
-         * <p/>
-         * Fortunately parsing is easy:  constructor takes the JSON string and converts it
-         * into an Object hierarchy for us.
-         */
-        private String[] getArtistsDataFromJson(String artistsJsonStr)
-                throws JSONException {
-
-            // These are the names of the JSON objects that need to be extracted.
-            final String OWM_ARTISTS = "artists";
-            final String OWM_ITEMS = "items";
-            final String OWM_NAME = "name";
-
-            JSONObject artistsJson = new JSONObject(artistsJsonStr);
-            JSONObject artistsObj = artistsJson.getJSONObject(OWM_ARTISTS);
-            JSONArray artistsArray = artistsObj.getJSONArray(OWM_ITEMS);
-
-            String[] resultStrs = new String[artistsArray.length()];
-            for (int i = 0; i < artistsArray.length(); i++) {
-
-                String name;
-
-                // Get the JSON object representing the day
-                JSONObject artistObject = artistsArray.getJSONObject(i);
-                name = artistObject.getString(OWM_NAME);
-
-                resultStrs[i] = name;
-            }
-            return resultStrs;
-
-        }
-
         @Override
-        protected String[] doInBackground(String... params) {
+        protected List<Artist> doInBackground(String... params) {
 
             if (params.length == 0) {
                 return null;
             }
 
-            // These two need to be declared outside the try/catch
-            // so that they can be closed in the finally block.
-            HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
 
-            // Will contain the raw JSON response as a string.
-            String artistsJsonStr = null;
+            SpotifyApi api = new SpotifyApi();
 
-            String type = "artist";
+// Most (but not all) of the Spotify Web API endpoints require authorisation.
+// If you know you'll only use the ones that don't require authorisation you can skip this step
+            // api.setAccessToken("myAccessToken");
 
-            try {
+            SpotifyService spotify = api.getService();
 
-                final String SPOTIFY_BASE_URL =
-                        "https://api.spotify.com/v1/search?";
-                final String QUERY_PARAM = "q";
-                final String FORMAT_PARAM = "mode";
-                final String UNITS_PARAM = "units";
-                final String DAYS_PARAM = "cnt";
-                final String TYPE_PARAM = "type";
-
-                Uri builtUri = Uri.parse(SPOTIFY_BASE_URL).buildUpon()
-                        .appendQueryParameter(QUERY_PARAM, params[0])
-                        .appendQueryParameter(TYPE_PARAM, type)
-                        .build();
-
-
-                URL url = new URL(builtUri.toString());
-
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-                // Read the input stream into a String
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuffer buffer = new StringBuffer();
-                if (inputStream == null) {
-                    // Nothing to do.
-                    return null;
-                }
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                    // But it does make debugging a *lot* easier if you print out the completed
-                    // buffer for debugging.
-                    buffer.append(line + "\n");
-                }
-
-                if (buffer.length() == 0) {
-                    // Stream was empty.  No point in parsing.
-                    return null;
-                }
-                artistsJsonStr = buffer.toString();
-            } catch (IOException e) {
-                Log.e(LOG_TAG, "Error ", e);
-                return null;
-            } finally {
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (final IOException e) {
-                        Log.e(LOG_TAG, "Error closing stream", e);
-                    }
-                }
-            }
-
-            try {
-                return getArtistsDataFromJson(artistsJsonStr);
-            } catch (JSONException e) {
-                Log.e(LOG_TAG, e.getMessage(), e);
-                e.printStackTrace();
-            }
-
-            return null;
+            ArtistsPager artistsPager = spotify.searchArtists(params[0]);
+            return artistsPager.artists.items;
         }
 
         @Override
-        protected void onPostExecute(String[] result) {
+        protected void onPostExecute(List<Artist> result) {
             if (result != null) {
                 mArtistsAdapter.clear();
-                for (String dayForecastStr : result) {
-                    mArtistsAdapter.add(dayForecastStr);
-                }
+                mArtistsAdapter.addAll(result);
                 // New data is back from the server.  Hooray!
             }
         }
